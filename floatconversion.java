f6e09526311e8c7d7c0a298f82fcb27c61c26946
gowsik-kannan <gowsikannan98@gmail.com>
2018-05-29 11:59:50 +0530

    oder

---
+++ floatconversion.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+	public static void main(String s[]) {
+	float degree=32.5f;
+	float value= degree - 2.3f;
+System.out.println("the value is "+value+"");	
+		
+	}
+}
---
+++ type.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static void main(String s[]) {
+		
+		byte a=10;
+		byte b=20;
+		byte c= (byte)(a+b);//case
+		System.out.println(c);
+	}
+}
