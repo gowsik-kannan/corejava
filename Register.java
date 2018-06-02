bb96ffc94c41300bb3b6658556b0c69ce9ce0ac9
gowsik-kannan <gowsikannan98@gmail.com>
2018-06-02 13:15:14 +0530

    changes

--- Register.java
+++ Register.java
@@ -1,10 +1,10 @@
 package com.student.org;
 
 public class Register {
-	public  void details(Student student1)
+	/*public  void details(Student student1)
 	{
 	 System.out.println(student1);
-	}
+	} */
 	public static void main(String[] args) {
 		Student student1= new Student();
 		student1.setBranch("B-Tech");
@@ -12,9 +12,9 @@ public class Register {
 		student1.setFistName("gowsik");
 		student1.setLastName("kannan");
 		student1.setGender("male");
-		Register r1=new Register();
-		r1.details(student1);
-		
+		//Register r1=new Register();
+		//r1.details(student1);
+		System.out.println(student1.toString());
 	}
 
 }
